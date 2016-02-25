43424d09c22699e85a1b7e37a15c33c3b664e50b
mafi-hamyarbott <mohammadhamyar24@gmail.com>
2016-02-25 17:49:12 +0330

    Update nod32bot.lua

--- bot/nod32bot.lua
+++ bot/nod32bot.lua
@@ -251,8 +251,8 @@ function create_config( )
     about_text = [[
 https://github.com/BH-YAGHI/NOD32-BOT.git
 
-channel : @Nod32team
-sodu : @behrooZyaghi
+channel : @hamyargroup
+sodu : @mhmafi
 ]],
     help_text_realm = [[
 Realm Commands:
@@ -317,11 +317,11 @@ Only sudo users can run this command
 !bc 123456789 Hello !
 This command will send text to [group_id]
 
-ch: @Nod32team
+ch: @hamyargroupp
 
 ]],
     help_text = [[
-NOD32 Commands list :
+hamyargroup Commands list :
  
 1-banhammer list ^
  
@@ -518,7 +518,7 @@ NOD32 Commands list :
 you can use both "/" and "!" 
 .شما میتوانید از ! و / استفاده کنید
 
-channel: @Nod32Team
+channel: @hamyargroupp
 
 G00D LUCK ^_^
 ]]
